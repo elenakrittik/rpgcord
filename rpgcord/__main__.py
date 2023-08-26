f7ffa0ce2f23835fd8819918e8dacc7a6c326be1
CatC0dd3r <catc0dd3r@gmail.com>
2023-08-26 21:17:01 +0300

    Basic database

--- rpgcord/__main__.py
+++ rpgcord/__main__.py
@@ -4,9 +4,12 @@ import asyncio
 from .bot import RPGcord
 from .config import config
 import contextlib
+from .database import create_base_table
 
 
 async def main() -> None:
+    await create_base_table()
+
     bot = RPGcord(intents = disnake.Intents.all())
     bot.load_extensions("./rpgcord/plugins")
     bot.i18n.load("./locale")  # type: ignore[reportUnknownMemberType]
---
+++ rpgcord/database.py
@@ -0,0 +1,18 @@
+import asyncmy
+from .config import config
+
+
+async def create_base_table() -> None:
+    con = await asyncmy.connect(host = config.db.connaddr,
+                                port = config.db.connport,
+                                user = config.db.username,
+                                password = config.db.password,
+                                db = config.db.database)
+
+    async with con.cursor() as cur:
+        await cur.execute("""CREATE TABLE IF NOT EXISTS users
+                          (id INT,
+                          name VARCHAR(255));""")
+        await con.commit()
+
+    con.close()
